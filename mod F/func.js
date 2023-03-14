f61abe6b5e2e1c5b79bee0b47edc7f638a19e97c
Diego Irigoyen <irigoyendiego92@gmail.com>
2023-03-14 14:25:59 -0300

    inicialização de funções

---
+++ mod F/func.js
@@ -0,0 +1,10 @@
+function parImp(n){
+    if(n%2==0){
+        return 'Par!'
+    } else {
+        return 'ímpar!'
+    }
+}
+
+let res = parImp(2)
+console.log(res)
---
+++ mod F/func2.js
@@ -0,0 +1,7 @@
+function soma(n1=0, n2=0){
+    return n1 + n2
+}
+
+// let res = soma(7, 3)
+let res = soma(7)
+console.log(res)
